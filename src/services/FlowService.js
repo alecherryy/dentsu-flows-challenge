34f6b7e5944b7ae477e583f45838054cc6fcaed2
Alessia Pizzoccheri <pizzoccheri.alessia5@gmail.com>
2021-04-02 12:23:09 -0400

    call Flows API and create FlowSelector component

---
+++ src/services/FlowService.js
@@ -0,0 +1,30 @@
+const URL = 'https://orchestrationflow.azurewebsites.net/flow';
+
+/**
+ * FIND ALL FLOWS
+ */
+export const findAllFlows = () => {
+  return fetch(`${URL}`).then(response => response.json());
+}
+
+/**
+ * FIND PROCESS BY FLOW
+ *
+ * @param {number} flowId
+ */
+export const findProcessByFlow = (flowId) => {
+  return fetch(`${URL}/${flowId}`).then(response => response.json());
+}
+
+/**
+ * FIND PROCESS BY ID
+ *
+ * @param {number} processId
+ */
+export const findProcessById = (processId) => {
+  return fetch(`${URL}/process/${processId}`).then(response => response.json());
+}
+
+export const API = {
+  findAllFlows, findProcessByFlow, findProcessById
+}
